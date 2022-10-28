384c13182ed8934efea608c14088fb5d8783350a
Oleksii Fedorenko <oleksii.fedorenko.94@gmail.com>
2022-10-28 15:48:30 +0400

    Add more tests to a project

--- test/specs/isNumberEven.spec.js
+++ test/specs/isNumberEven.spec.js
@@ -1,5 +1,5 @@
 const NumbersValidator = require('../../app/numbers_validator.js');
-const {expect} = require('chai');
+const {expect, assert} = require('chai');
 
 describe('isNumberEven positive scenarios', () => {
   let validator;
@@ -9,10 +9,52 @@ describe('isNumberEven positive scenarios', () => {
   afterEach(() => {
     validator = undefined;
   });
+
   it('Should return true if number is even', () => {
     expect(validator.isNumberEven(4)).to.be.equal(true);
   });
-  it('Should return false if number if odd', () => {
-    expect(validator.isNumberEven(5)).to.be.equal(false);
+  it('Should return true if number is 0', () => {
+    expect(validator.isNumberEven(0)).to.be.equal(true);
+  });
+});
+
+describe('isNumberEven negative scenatios', () =>{
+  let validator;
+  beforeEach(() => {
+    validator = new NumbersValidator();
+  });
+  afterEach(() => {
+    validator = undefined;
+  });
+
+  it('Should return false if number is not even', () => {
+    expect(validator.isNumberEven(5))
+        .to.equal(false);
+  });
+
+  it('Should throw an error if the type of variable is string', () => {
+    assert.throws(() => {
+      validator.isNumberEven('a');
+    }, Error, 'a is not of type "Number". It is of type string');
+  });
+
+  it('Should throw an error if the type of variable is null', () => {
+    assert.throws(() => {
+      validator.isNumberEven(null);
+    }, Error, 'null is not of type "Number". It is of type object');
+  });
+});
+
+describe('getEvenNumbersFromArray positive scenarios', () => {
+  let validator;
+  beforeEach(() => {
+    validator = new NumbersValidator();
+  });
+  afterEach(() => {
+    validator = undefined;
+  });
+
+  it('Should return an array of even numbers', () => {
+    expect(validator.getEvenNumbersFromArray([1, 2, 3, 4, 5])).to.eqls([2, 4]);
   });
 });
